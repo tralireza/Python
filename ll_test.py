cbc2b554118fce59e07aa9730c87194275d4165d
Alireza <26859725+tralireza@users.noreply.github.com>
2024-09-10 19:37:32 +0100

    Linked List 2807m Insert Greatest Common Divisors in Linked List

---
+++ ll_test.py
@@ -0,0 +1,18 @@
+import ll
+
+# 2807m Insert Greatest Common Divisors in Linked List
+def test_Solution2807():
+   from ll import ListNode as L
+
+   def draw(ls):
+      s = ""
+      while ls:
+         if ls.next:
+            s += "{" + str(ls.val) + " *}->"
+         else:
+            s += "{" + str(ls.val) + " /}"
+         ls = ls.next
+      return s
+
+   ls = L(18, L(6, L(10, L(3))))
+   print(draw(ls), " => ", draw(ll.Solution2807().insertGreatestCommonDivisors(ls)))
