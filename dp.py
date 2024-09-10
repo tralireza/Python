34b268fe3e5f116ad9da3ee86a2e6de5e5311068
Alireza <26859725+tralireza@users.noreply.github.com>
2024-09-10 21:03:22 +0100

    Dynamic Programming 221m Maximal Square

--- dp.py
+++ dp.py
@@ -1,3 +1,6 @@
+from typing import List
+
+# 97m Interleaving String
 class Solution97:
    def isInterleaving(self, s1: str, s2: str, s3: str) -> bool:
       R, C = len(s1), len(s2)
@@ -19,3 +22,20 @@ class Solution97:
             dp[r][c] = dp[r-1][c] and s1[r-1] == s3[r+c-1] or dp[r][c-1] and s2[c-1] == s3[r+c-1]
 
       return dp[R][C]
+
+# 221m Maximal Square
+class Solution221:
+   def maximalSquare(self, matrix: List[List[str]]) -> int:
+      Rows, Cols = len(matrix), len(matrix[0])
+      dp = [[0]*(Cols+1) for _ in range(Rows+1)]
+
+      x = 0
+      for r in range(Rows):
+         for c in range(Cols):
+            if matrix[r][c] == "1":
+               dp[r+1][c+1] = 1 + min(dp[r][c], dp[r+1][c], dp[r][c+1])
+               x = max(dp[r+1][c+1], x)
+
+      print(dp)
+
+      return x * x
--- dp_test.py
+++ dp_test.py
@@ -4,3 +4,8 @@ def test_Solution97():
    assert dp.Solution97().isInterleaving("", "", "") == True
    assert dp.Solution97().isInterleaving("aabcc", "dbbca", "aadbbcbcac") == True 
    assert dp.Solution97().isInterleaving("aabcc", "dbbca", "aadbbbaccc") == False
+
+# 221m Maximal Square
+def test_solution221():
+   assert dp.Solution221().maximalSquare([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]) == 4
+   assert dp.Solution221().maximalSquare([["0","1"],["1","0"]]) == 1
