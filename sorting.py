3e6aba66c873149004571cd288d6058233aab728
Alireza <26859725+tralireza@users.noreply.github.com>
2024-09-17 09:57:55 +0100

    Sorting 539m Minimum Time Difference

---
+++ sorting.py
@@ -0,0 +1,18 @@
+""" Sorting Solutions """
+from typing import List
+
+
+class Solution539():
+    """
+    539m Minimum Time Difference
+    """
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        Ms = [60 * int(t[:2]) + int(t[3:]) for t in timePoints]
+        Ms.sort()
+
+        print(Ms)
+
+        r = 720
+        for i in range(len(Ms)-1):
+            r = min(Ms[i+1] - Ms[i], r)
+        return min(1440 - (Ms[-1] - Ms[0]), r)
---
+++ sorting_test.py
@@ -0,0 +1,7 @@
+""" tests for Sorting module """
+import sorting as m
+
+
+def test_Solution539():
+    assert m.Solution539().findMinDifference(["23:59", "00:00"]) == 1
+    assert m.Solution539().findMinDifference(["00:00", "23:59", "00:00"]) == 0
