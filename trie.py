1b4dc3bc851fe397ce96388dd3d3f2596a67a50d
Alireza <26859725+tralireza@users.noreply.github.com>
2024-09-25 13:05:37 +0100

    Trie 246h Sum of Prefix Scores of Strings

--- trie.py
+++ trie.py
@@ -5,6 +5,50 @@ Trie
 from typing import List
 
 
+class Solution2416:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        """
+        2416h Sum of Prefix Scores of Strings
+        """
+        class trie:
+            def __init__(self):
+                self.child = [None]*26
+                self.score = 0
+
+        t = trie()
+
+        def insert(word: str):
+            n = t
+            for letter in word:
+                c = n.child[ord(letter)-ord('a')]
+                if not c:
+                    c = trie()
+                    n.child[ord(letter)-ord('a')] = c
+                n = c
+                n.score += 1
+
+        def search(word: str) -> int:
+            score = 0
+            n = t
+            for letter in word:
+                c = n.child[ord(letter)-ord('a')]
+                if not c:
+                    return 0
+                n = c
+                score += n.score
+            return score
+
+        for word in words:
+            insert(word)
+
+        print(" ->", t.child)
+
+        scores = []
+        for word in words:
+            scores.append(search(word))
+        return scores
+
+
 class Solution3043:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
         """
--- trie_test.py
+++ trie_test.py
@@ -1,6 +1,12 @@
 import trie as m
 
 
+def test_Solution2416():
+    # 2416h Sum of Prefix Scores of Strings
+    assert m.Solution2416().sumPrefixScores(["abc", "ab", "bc", "b"]) == [5, 4, 3, 2]
+    assert m.Solution2416().sumPrefixScores(["abcd"]) == [4]
+
+
 def test_Solution3043():
     # 3043m Find the Length of the Longest Common Prefix
     assert m.Solution3043().longestCommonPrefix([1, 10, 100], [1000]) == 3
