99ad46395f5452cc118170574e4fbfad39fae48c
Alireza <26859725+tralireza@users.noreply.github.com>
2024-11-20 18:26:35 +0000

    Sliding Window 2461m Maximum Sum of Distinct Subarrays With Length K

--- slidingwindow.py
+++ slidingwindow.py
@@ -12,7 +12,7 @@ class Solution1652:
 
         start, end = 1, k
         if k < 0:
-            start, end = len(code) + k, len(code)-1
+            start, end = len(code) + k, len(code) - 1
 
         ksum = 0
         for i in range(start, end+1):
@@ -28,3 +28,28 @@ class Solution1652:
             start = (start+1) % len(code)
 
         return rst
+
+
+class Solution2461:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        """
+        2461m Maximum Sum of Distinct Subarrays With Length K
+        """
+
+        xsum, ksum = 0, 0
+        last = {}
+
+        start = 0
+        for end, n in enumerate(nums):
+            ksum += n
+
+            n_last = last.get(n, -1)
+            while start <= n_last or end - start + 1 > k:
+                ksum -= nums[start]
+                start += 1
+            last[n] = end
+
+            if end - start + 1 == k:
+                xsum = max(ksum, xsum)
+
+        return xsum
--- slidingwindow_test.py
+++ slidingwindow_test.py
@@ -7,3 +7,10 @@ def test_Solution1652():
     assert m.Solution1652().decrypt([5, 7, 1, 4], 3) == [12, 10, 16, 13]
     assert m.Solution1652().decrypt([1, 2, 3, 4], 0) == [0, 0, 0, 0]
     assert m.Solution1652().decrypt([2, 4, 9, 3], -2) == [12, 5, 6, 13]
+
+
+def test_Solution2461():
+    # 2461m Maximum Sum of Distinct Subarrays With Length K
+
+    assert m.Solution2461().maximumSubarraySum([1, 5, 4, 2, 9, 9, 9], 3) == 15
+    assert m.Solution2461().maximumSubarraySum([4, 4, 4], 3) == 0
