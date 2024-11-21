53b72aa102be64996fac9fd943ad809ebad4a004
Alireza <26859725+tralireza@users.noreply.github.com>
2024-11-21 00:36:42 +0000

    Sliding Window 2516m Take K of Each Character From Left to Right

--- slidingwindow.py
+++ slidingwindow.py
@@ -53,3 +53,39 @@ class Solution2461:
                 xsum = max(ksum, xsum)
 
         return xsum
+
+
+class Solution2516:
+    def takeCharacters(self, s: str, k: int) -> int:
+        """
+        2516m Tale K of Each Character From Left and Right
+        """
+
+        if k == 0:
+            return 0
+
+        from collections import Counter
+        count = Counter(s)
+        if len(count) < 3:
+            return -1
+        for letter in count:
+            if count[letter] < k:
+                return -1
+
+        window = Counter()
+        xwin, left = 0, 0
+
+        for right, letter in enumerate(s):
+            window[letter] += 1
+
+            while left <= right and (
+                count["a"] - window["a"] < k or
+                count["b"] - window["b"] < k or
+                count["c"] - window["c"] < k
+            ):
+                window[s[left]] -= 1
+                left += 1
+
+            xwin = max(xwin, right - left + 1)
+
+        return len(s) - xwin
--- slidingwindow_test.py
+++ slidingwindow_test.py
@@ -14,3 +14,10 @@ def test_Solution2461():
 
     assert m.Solution2461().maximumSubarraySum([1, 5, 4, 2, 9, 9, 9], 3) == 15
     assert m.Solution2461().maximumSubarraySum([4, 4, 4], 3) == 0
+
+
+def test_Solution2516():
+    # 2516m Take K of Each Character From Left or Right
+
+    assert m.Solution2516().takeCharacters("aabaaaacaabc", 2) == 8
+    assert m.Solution2516().takeCharacters("a", 1) == -1
